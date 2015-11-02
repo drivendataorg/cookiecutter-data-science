f90e83a9686e6d8ced90f7cde89c64e755a5b43a
isms <isaac.slavitt@gmail.com>
2015-11-02 09:09:45 -0500

    Add support for .env loading

--- {{ cookiecutter.repo_name }}/src/make_dataset.py
+++ {{ cookiecutter.repo_name }}/src/make_dataset.py
@@ -4,8 +4,8 @@ import logging
 
 
 @click.command()
-@click.argument('input_filepath', type=str)
-@click.argument('output_filepath', type=str)
+@click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
@@ -14,6 +14,10 @@ def main(input_filepath, output_filepath):
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+    dotenv_path = os.path.join(project_dir, '.env')
+    dotenv.load_dotenv(dotenv_path)
 
     main()
 
