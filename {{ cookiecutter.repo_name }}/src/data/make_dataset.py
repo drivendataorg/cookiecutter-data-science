76930056bf6e47cc05ae5305d14a19eb0cd0e878
Peter Bull <pjbull@gmail.com>
2016-09-05 19:57:24 -0400

    Add docstring in make_dataset; closes #1

--- {{ cookiecutter.repo_name }}/src/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/src/data/make_dataset.py
@@ -9,6 +9,9 @@ from dotenv import find_dotenv, load_dotenv
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../processed).
+    """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
