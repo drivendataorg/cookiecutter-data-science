d13bc88ab0398195db466f99e2f58e31f1e241fb
Colin Sullivan <csulliva@brandeis.edu>
2016-10-05 15:16:42 -0400

    add parameter to specify python interpreter and commands to create and test virtual environments

---
+++ {{ cookiecutter.repo_name }}/test_environment.py
@@ -0,0 +1,24 @@
+import sys
+
+REQUIRED_PYTHON = "{{ cookiecutter.python_interpreter }}"
+
+
+def main():
+    system_major = sys.version_info.major
+    if REQUIRED_PYTHON == "python":
+        required_major = 2
+    elif REQUIRED_PYTHON == "python3":
+        required_major = 3
+    else:
+        raise ValueError("Unrecognized python interpreter: {}".format(
+            REQUIRED_PYTHON))
+
+    if system_major != required_major:
+        raise TypeError("This project requires Python {}. Found: {}".format(
+            required_major, sys.version))
+    else:
+        print(">>> Development environment passes all tests!")
+
+
+if __name__ == '__main__':
+    main()
