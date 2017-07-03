f1de83fef48538311c51e02bf17ff17074086e2d
Andy Reagan <andy@andyreagan.com>
2017-07-03 15:04:45 -0400

    Added hooks to remove R or python code

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,33 @@
 import os
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+import subprocess
 
 # Remove R stuff if not going to be used
 if '{{ cookiecutter.include_r_boilerplate }}'.lower() == 'false':
-    os.remove(os.path.join(
-        PROJECT_DIRECTORY, 'src','data','R','utils.R'
-    ))
-    os.remove(os.path.join(
-        PROJECT_DIRECTORY, 'src','data','make_dataset.R'
-    ))
-    os.rmdir(os.path.join(
-        PROJECT_DIRECTORY, 'src','data','R'
-    ))
+    subprocess.call("rm -r src/*/*R",shell=True)
+    # os.remove(os.path.join(
+    #     PROJECT_DIRECTORY, 'src','data','R','utils.R'
+    # ))
+    # os.remove(os.path.join(
+    #     PROJECT_DIRECTORY, 'src','data','make_dataset.R'
+    # ))
+    # os.remove(os.path.join(
+    #     PROJECT_DIRECTORY, 'src','features','build_features.R'
+    # ))
+    # os.remove(os.path.join(
+    #     PROJECT_DIRECTORY, 'src','models','test_model.R'
+    # ))
+    # os.remove(os.path.join(
+    #     PROJECT_DIRECTORY, 'src','models','train_model.R'
+    # ))
+    # os.remove(os.path.join(
+    #     PROJECT_DIRECTORY, 'src','vis','visualize.R'
+    # ))
+    # for folder in ["data","exploratory","features","models","vis"]:
+    #     os.rmdir(os.path.join(
+    #         PROJECT_DIRECTORY, 'src',folder,'R'
+    #     ))
+
+# Remove python stuff if not going to be used
+if '{{ cookiecutter.include_python_boilerplate }}'.lower() == 'false':
+    subprocess.call("rm -r src/*/*.py",shell=True)    
