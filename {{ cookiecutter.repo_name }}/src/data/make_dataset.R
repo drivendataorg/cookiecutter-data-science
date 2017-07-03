e4c79b19f68a4c3ebac54b2d9f9160ad550d2c3c
Andy Reagan <andy@andyreagan.com>
2017-07-03 15:09:15 -0400

    Added R code for connecting the vertica with mmlib

--- {{ cookiecutter.repo_name }}/src/data/make_dataset.R
+++ {{ cookiecutter.repo_name }}/src/data/make_dataset.R
@@ -5,3 +5,8 @@ library(devtools)
 
 # load package w/o installing
 load_all('R')
+
+# get vertica connection
+# make sure you've sourced the root .env file first
+# NOTE: first install mmlib!
+con <- mmlib::vertica_connect(Sys.getenv("user"), Sys.getenv("pw"), "jdbc:vertica://vertica:5433/advana")
