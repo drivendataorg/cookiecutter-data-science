8c391b8502bb8b022df111eaf5e5beeda5eb69b5
Andy Reagan <andy@andyreagan.com>
2017-12-19 15:06:18 -0500

    More boilerplate for connecting to Vertica in Python

--- {{cookiecutter.repo_name}}/src/data/make_dataset.py
+++ {{cookiecutter.repo_name}}/src/data/make_dataset.py
@@ -3,18 +3,29 @@ import os
 import click
 import logging
 from dotenv import find_dotenv, load_dotenv
+import yaml
 import ssl
 import vertica_python
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+
+def run_from_ipython():
+    try:
+        __IPYTHON__
+        return True
+    except NameError:
+        return False
+
+
+if run_from_ipython():
+    from IPython import get_ipython
+    ipython = get_ipython()
+    ipython.magic("matplotlib inline")
+    project_dir = os.getcwd()
+else:
+    project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+src_dir = os.path.join(project_dir, "src")
+sys.path.append(src_dir)
+
 
 def get_vertica_python_conn(cfg=None):
     cfg = cfg or default_cfg
@@ -30,9 +41,54 @@ def get_vertica_python_conn(cfg=None):
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
         ssl_context.verify_mode = ssl.CERT_NONE
         ssl_context.check_hostname = False
-        params['ssl']=ssl_context
+        params['ssl'] = ssl_context
     conn = vertica_python.connect(**params)
     return conn
+
+
+def vertica_python_conn(config, account='user', server='vertica'):
+    """
+    Generate vertica_python configuration object from configuration.
+
+    Args:
+    config -- database configuation.
+
+    Returns:
+    conn -- database connection.
+    """
+    params = {
+        'host': config[server]['host'],
+        'port': 5433,
+        'database': config[server]['database'],
+        'read_timeout': 10 * 60 * 60,
+        'unicode_error': 'strict',
+        'user': config[server][account]['username'],
+        'password': config[server][account]['password']
+    }
+    conn = vertica_python.connect(**params)
+    return conn
+
+
+def vertica_python_conn_wrapper(**kwargs):
+    conn_info = yaml.safe_load(open(find_dotenv('.config.yml')))
+    return vertica_python_conn(conn_info, **kwargs)
+
+
+@click.command()
+@click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('output_filepath', type=click.Path())
+def main(input_filepath, output_filepath):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../processed).
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from raw data')
+    with open(input_filepath, "r") as f:
+        query = f.read()
+
+    df = pd.read_sql_query(query, con)
+    df.to_pickle(output_filepath)
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -59,5 +115,9 @@ if __name__ == '__main__':
                  'ssl': True}
 
     con = get_vertica_python_conn(conn_info)
+
+    # OR
+
+    con = vertica_python_conn_wrapper()
 
     main()
