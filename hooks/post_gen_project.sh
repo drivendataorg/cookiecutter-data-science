aaf4fca063b2c8601e17cde55be92bfe03770f34
Matyas Tamas <matyas@manifold.ai>
2018-01-23 11:21:48 -0500

    hooks: add support for aws_profiles and 'set -e' in post_gen

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
+set -e
 
 # Initial sync of data folder to project specific prefix under the manifold-projects bucket
 echo "Creating shared data folders at s3://manifold-projects/{{ cookiecutter.repo_name }}..."
-aws s3 sync data s3://manifold-projects/{{ cookiecutter.repo_name }} 
+aws s3 sync data s3://manifold-projects/{{ cookiecutter.repo_name }} --profile {{ cookiecutter.aws_profile }}
 echo "Done."
 
-# Log in to ECR 
+# Log in to ECR
 echo "Performing docker login to AWS container registry..."
-aws ecr get-login --no-include-email | sh
+aws ecr get-login --no-include-email --profile {{ cookiecutter.aws_profile }}| sh
 echo "Done."
 
 echo "All set! Run the start.sh script and open Kitematic from the Docker minibar menu to make sure your container is running the Jupyter service."
