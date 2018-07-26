5e2c0b6d14492a5f3ae10d9d9f9d6f1b016bad11
dmitrypolo <dmitrypolo90@gmail.com>
2018-07-26 15:50:38 -0400

    change hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,4 +3,6 @@ import shutil
 
 CWD = os.getcwd()
 if '{{ cookiecutter.include_starter_proj }}' == 'N':
-    shutil.rmtree(os.path.join(CWD, 'sample_project'))
+    shutil.rmtree(os.path.join(CWD, 'tests'))
+    open(os.path.join(CWD, 'src/data/make_dataset.py'), 
+        'w').close()
