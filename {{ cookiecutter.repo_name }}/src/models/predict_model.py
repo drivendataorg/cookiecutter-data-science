756ed6824c606c1cfdfc8c88068ed6e9c6b3a12a
dmitrypolo <dmitrypolo90@gmail.com>
2018-07-27 12:59:42 -0400

    clean up unused imports and use PEP

--- {{ cookiecutter.repo_name }}/src/models/predict_model.py
+++ {{ cookiecutter.repo_name }}/src/models/predict_model.py
@@ -2,9 +2,8 @@ import os
 import pickle
 import pandas as pd
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 from train_model import ROOT
+
 
 def retrieve_model():
     pickled_model = os.path.join(ROOT, 'models/titanic.model')
@@ -22,10 +21,9 @@ def main():
         'data/processed/titanic_y_test.csv'), header=None)
     print(f'The model returned these predictions:\n{y_pred}')
 
-    #clf = LogisticRegression(penalty='l1')
-    #logisticRegr.fit(x_train, y_train.astype(int))
-    auc = roc_auc_score(y_test.astype(int), deserialized_model.predict_proba(X_test)[:,1])
-    print('AUC (area under ROC curve): '+str(auc))
+    auc = roc_auc_score(y_test.astype(int), deserialized_model.predict_proba(X_test)[:, 1])
+    print('AUC (area under ROC curve): ' + str(auc))
+
 
 if __name__ == '__main__':
     main()
