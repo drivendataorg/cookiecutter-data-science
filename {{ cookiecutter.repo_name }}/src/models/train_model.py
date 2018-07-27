bf32822c617eb569cef2ff52ee6084da7acff555
dmitrypolo <dmitrypolo90@gmail.com>
2018-07-27 11:02:31 -0400

    rename path

--- {{ cookiecutter.repo_name }}/src/models/train_model.py
+++ {{ cookiecutter.repo_name }}/src/models/train_model.py
@@ -27,7 +27,6 @@ def fetch_processed(data_path):
 
 def fit_model(X_train, y_train):
     model = RandomForestClassifier(n_estimators=100)
-    print(type(model))
     # Fit to the training data
     model.fit(X_train, y_train)
     return model
@@ -36,9 +35,9 @@ def fit_model(X_train, y_train):
 def main():
     x_train, x_test, y_train, _ = fetch_processed('data/processed/titanic.csv')
     model = fit_model(x_train, y_train)
-    model_out_dir = os.path.join(ROOT, 'models/titanic.model')
+    model_out_path = os.path.join(ROOT, 'models/titanic.model')
     test_out_dir = os.path.join(ROOT, 'data/processed/titanic_test.csv')
-    with open(model_out_dir, 'wb') as fout:
+    with open(model_out_path, 'wb') as fout:
         pickle.dump(model, fout, PROTOCOL)
     x_test.to_csv(test_out_dir, index=False)
 
