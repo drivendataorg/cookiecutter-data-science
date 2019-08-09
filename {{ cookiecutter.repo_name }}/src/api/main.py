b6496bddd6fa85d71c562c21bf9f28252b4047b4
joandre <joandre@users.noreply.github.com>
2019-08-09 17:29:43 +0200

    Add CORS for HTTPS management

--- {{ cookiecutter.repo_name }}/src/api/main.py
+++ {{ cookiecutter.repo_name }}/src/api/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+# Beware to origins allowed ! In this template by default every IP can reach your API !
+from starlette.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
