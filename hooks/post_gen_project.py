d7c14982a9509ea09a08829830683dcfde3eb72e
davebulaval <david.beauchemin.5@ulaval.ca>
2020-02-25 17:42:06 -0500

    moved test url to post

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,6 +5,12 @@ import sys
 conda_env_creation = True if "{{ cookiecutter.create_conda_env }}" == "Yes" else False
 data_version_control = True if "{{ cookiecutter.DVC_setting }}" == "Yes" else False
 
+
+try:
+    subprocess.run(["make", "test_remote_url"], check=True)
+except subprocess.CalledProcessError:
+    sys.exit(1)
+
 if conda_env_creation:
     try:
         subprocess.run(["make", "init_environment"], check=True)
--- hooks/pre_gen_project.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import subprocess
-import sys
-
-try:
-    subprocess.run(["make", "test_remote_url"], check=True)
-except subprocess.CalledProcessError:
-    sys.exit(1)
