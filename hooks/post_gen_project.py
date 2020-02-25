03ef4cb4edcecbf024c3e607a125379701da41d1
davebulaval <david.beauchemin.5@ulaval.ca>
2020-02-25 15:37:29 -0500

    call direcly make function

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import subprocess
+import os
 
 data_version_control = True if "{{ cookiecutter.DVC_setting }}" == "Yes" else False
 conda_env_creation = True if "{{ cookiecutter.create_conda_env }}" == "Yes" else False
@@ -8,4 +8,4 @@ if data_version_control:
     pass
 
 if conda_env_creation:
-    print(subprocess.call(["./create_env.sh"]))
+    print(os.system("make create_environment"))
