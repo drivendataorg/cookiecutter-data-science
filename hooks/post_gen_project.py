a152e2f43871636b197a3ab36d645a04dc23e3d2
davebulaval <david.beauchemin.5@ulaval.ca>
2020-02-25 16:13:04 -0500

    try except and sys exit on fail

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import subprocess
+import sys
 
 data_version_control = True if "{{ cookiecutter.DVC_setting }}" == "Yes" else False
 conda_env_creation = True if "{{ cookiecutter.create_conda_env }}" == "Yes" else False
@@ -8,5 +9,7 @@ if data_version_control:
     pass
 
 if conda_env_creation:
-    exit_status = subprocess.run(["make", "create_environment"])
-    print(exit_status.returncode)
+    try:
+        exit_status = subprocess.run(["make", "create_environment"], check=True)
+    except subprocess.CalledProcessError:
+        sys.exit(1)
