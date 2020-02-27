82de3644456c31caa60db461866b80503a62eb24
Jay Qi <jayqi@users.noreply.github.com>
2020-02-27 14:47:02 -0800

    Add missing env directory bin/activate path

--- tests/virtualenv_harness.sh
+++ tests/virtualenv_harness.sh
@@ -9,7 +9,7 @@ function finish {
     if [[ $(which python) == *"$PROJECT_NAME"* ]]; then
         deactivate
     fi
-    
+
     rmvirtualenv $PROJECT_NAME
 }
 trap finish EXIT
@@ -17,21 +17,21 @@ trap finish EXIT
 # source the steps in the test
 source $CCDS_ROOT/test_functions.sh
 
-# navigate to the generated project and run make commands 
+# navigate to the generated project and run make commands
 cd $1
 
 TEMP_ENV_ROOT=$(mktemp -d "${TMPDIR:-/tmp/}$(basename $0).XXXXXXXXXXXX")
 export WORKON_HOME=$TEMP_ENV_ROOT
 
 # virtualenvwrapper.sh must be on the PATH on the test host machine,
-# which should be the case if virtualenvwrapper is pip installed in 
+# which should be the case if virtualenvwrapper is pip installed in
 # the base Python
 source $(which virtualenvwrapper.sh)
 
 make create_environment
 
 # workon not sourced
-. $TEMP_ENV_ROOT/bin/activate
+. $TEMP_ENV_ROOT/$PROJECT_NAME/bin/activate
 make requirements
 
 run_tests $PROJECT_NAME
