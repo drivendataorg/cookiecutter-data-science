5f48b47f16ec183956b6849bcc30b6962a5a25ad
Riley Brady <riley.brady@colorado.edu>
2020-03-11 12:07:55 -0600

    change src to project name

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='{{ cookiecutter.repo_name }}',
     packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
