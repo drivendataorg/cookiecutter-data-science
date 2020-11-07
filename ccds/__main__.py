ddc11ad4a4d1a847c660856d4ca40a8a5b12d1db
Peter Bull <pjbull@gmail.com>
2020-11-07 15:40:03 -0800

    Unremove used import

--- ccds/__main__.py
+++ ccds/__main__.py
@@ -11,16 +11,14 @@ from ccds.monkey_patch import generate_context_wrapper, prompt_for_config
 prompt.prompt_for_config = prompt_for_config
 
 
-# monkey-patch context to point to ccds.json file
+# monkey-patch context to point to ccds.json
 from cookiecutter import generate
 from ccds.monkey_patch import generate_context_wrapper
-
 generate.generate_context = generate_context_wrapper
-
 
 # for use in tests need monkey-patched api main
 from cookiecutter import cli
-
+from cookiecutter import main as api_main 
 main = cli.main
 
 
