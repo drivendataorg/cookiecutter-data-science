b6076f03dbc051dda61998c67df4bd904345bf02
Alicia <alicia.arenzana@gmail.com>
2020-11-30 16:59:27 +0100

    remove pytest option; making it a must

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,18 +14,6 @@ if __name__ == '__main__':
         remove_file('AUTHORS.rst')
         remove_file('docs/authors.rst')
 
-    # if '{{ cookiecutter.use_pytest }}' == 'y':
-    #     remove_file('tests/__init__.py')
-
-    # if '{{ cookiecutter.use_sqlalchemy }}' == 'n':
-    #     database = os.path.join(
-    #         '{{ cookiecutter.project_slug }}', 'core', 'database.py')
-    #     models = os.path.join(
-    #         '{{ cookiecutter.project_slug }}', 'models.py')
-    #     remove_file(database)
-    #     remove_file(models)
-    #     remove_file('tests/conftest.py')
-
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
         remove_file(cli_file)
