074b545a628b654176054a729a6de25920b74115
Alicia <alicia.arenzana@gmail.com>
2020-12-01 15:46:52 +0100

    added hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 import os
+import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+def remove_dir(filepath):
+    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, filepath), ignore_errors=False, onerror=None)
 
 
 if __name__ == '__main__':
@@ -18,5 +22,14 @@ if __name__ == '__main__':
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
         remove_file(cli_file)
 
-    if 'Not open source' == '{{ cookiecutter.open_source_license }}':
-        remove_file('LICENSE')
+    if 'n' == '{{ cookiecutter.notebooks|lower }}':
+        remove_dir('LICENSE')
+
+    if 'n' == '{{ cookiecutter.module|lower }}':
+        remove_dir('LICENSE')
+
+    if 'n' == '{{ cookiecutter.airflow_setup|lower }}':
+        remove_dir('LICENSE')
+
+    if 'n' == '{{ cookiecutter.cloud_functions|lower }}':
+        remove_dir('LICENSE')
