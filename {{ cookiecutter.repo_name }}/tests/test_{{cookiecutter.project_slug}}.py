d27b221749db20745f3fe3e195b63d2fc134095d
Alicia <alicia.arenzana@gmail.com>
2020-12-01 14:56:43 +0100

    make pytest the default

--- {{ cookiecutter.repo_name }}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{ cookiecutter.repo_name }}/tests/test_{{cookiecutter.project_slug}}.py
@@ -3,11 +3,8 @@
 
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 
-{% if cookiecutter.use_pytest == 'y' -%}
 import pytest
-{% else %}
-import unittest
-{%- endif %}
+
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 from click.testing import CliRunner
 {%- endif %}
@@ -16,8 +13,6 @@ from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 from {{ cookiecutter.project_slug }} import cli
 {%- endif %}
-
-{%- if cookiecutter.use_pytest == 'y' %}
 
 
 @pytest.fixture
@@ -45,31 +40,4 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
-{%- endif %}
-{%- else %}
-
-
-class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
-    """Tests for `{{ cookiecutter.project_slug }}` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert '{{ cookiecutter.project_slug }}.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
-{%- endif %}
 {%- endif %}
