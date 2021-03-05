a86267e02f42a8797ea584de6ca2cb2916bba7b2
Emily Miller <ejm714@gmail.com>
2021-03-05 10:27:37 -0800

    add ethics checklist to default files

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -85,6 +85,7 @@ def verify_files(root, config):
         "docs/getting-started.rst",
         "docs/index.rst",
         "docs/make.bat",
+        "ethics.md"
         "notebooks/.gitkeep",
         "notebooks/.nbautoexport",
         "references/.gitkeep",
