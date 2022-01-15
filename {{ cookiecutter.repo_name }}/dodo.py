8d4d0feebb3868c76707632e86cd414c10b0ff92
Kristoffer Johansson <10258920+krjoha@users.noreply.github.com>
2022-01-15 12:15:34 +0100

    Added 'black' for python code formatting.

--- {{ cookiecutter.repo_name }}/dodo.py
+++ {{ cookiecutter.repo_name }}/dodo.py
@@ -2,17 +2,20 @@ from doit.tools import LongRunning
 
 
 def task_pytest():
-    return {
-        'actions': ['pytest'],
-        'doc': 'runs tests with pytest'
-    }
+    return {"actions": ["pytest"], "doc": "runs tests with pytest"}
 
 
 def task_lint():
-    cmd = ['pylint --rcfile=setup.cfg **/*.py',
-           'flake8',
-           'bandit -r --ini setup.cfg']
+    cmd = [
+        "black .",
+        "pylint --rcfile=setup.cfg **/*.py",
+        "flake8",
+        "bandit -r --ini setup.cfg",
+    ]
 
     for c in cmd:
-        yield {'name': c.split()[0],
-               'actions': [LongRunning(c)]}
+        yield {
+            "name": c.split()[0],
+            "actions": [LongRunning(c)],
+            "doc": "runs black formatting and linting",
+        }
--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/__init__.py
@@ -7,12 +7,12 @@ from loguru import logger
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
-fmt = '{time:YYYY-MM-DD HH:mm:ss} | {level} | {name} :: {module} :: {function} :: {line} - {message}'
+fmt = "{time:YYYY-MM-DD HH:mm:ss} | {level} | {name} :: {module} :: {function} :: {line} - {message}"
 
 config = {
     "handlers": [
-        {"sink": sys.stdout, 'level': "DEBUG"},
-        {"sink": "log.log", "format": fmt, 'level': 'DEBUG'},
+        {"sink": sys.stdout, "level": "DEBUG"},
+        {"sink": "log.log", "format": fmt, "level": "DEBUG"},
     ]
 }
 
