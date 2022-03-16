867816a4d192dd6a80fd948e91113c16b854a674
Piotr Deszyński <pdeszynski@gmail.com>
2022-03-16 14:58:13 +0100

    fix(check-commits): corrected commit check

--- {{ cookiecutter.repo_name }}/.github/workflows/checkCommits.sh
+++ {{ cookiecutter.repo_name }}/.github/workflows/checkCommits.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
+set -e
+
 sourceBranch=${1:-$(git branch --show-current)}
 targetBranch=${2-origin/master}
+git fetch -p
 echo "Checking diff between $sourceBranch and $targetBranch"
-commits=$(git log --pretty=%B $sourceBranch...$targetBranch | grep -v -e '^$')
-echo -e "Checking commits:\n$commits"
-IFS='
-'
-exitCode=0
-for commit in $commits
-do
-    poetry run cz check -m "$commit"
-    lastExitCode="$?"
-    if [ $lastExitCode -ne 0 ] ; then
-        exitCode=$lastExitCode
-    fi
-done
-exit $exitCode
+poetry run cz check --rev-range $targetBranch..$sourceBranch
