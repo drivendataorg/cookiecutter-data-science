c2de5686067bccbbca77e43a194221c982c17e11
Carlos Wanderley <63980759+lcricco@users.noreply.github.com>
2022-10-15 00:34:30 -0700

    a

--- {{ cookiecutter.repo_name }}/make_files.py
+++ {{ cookiecutter.repo_name }}/make_files.py
@@ -9,7 +9,7 @@ else:
     dir_notes = "notebooks"
     files = os.listdir(dir_notes)
     files = [f for f in files if os.path.isfile(dir_notes+'/'+f)] #Filtering only the files.
-    # files.pop(0)
+    files.pop(0)
     # print(files)
     
     # create a file
