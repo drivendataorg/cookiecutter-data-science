1b4e8df5f0dfb424b67614becbd581fbfe76d19d
devparsa@gmail.com <devparsa@gmail.com>
2022-10-30 18:19:50 +0000

    Basic flow

---
+++ {{ cookiecutter.repo_name }}/src/folderB/fileB.py
@@ -0,0 +1,4 @@
+
+def fileB_func():
+    print("A function called in FileB")
+    return True
--- {{ cookiecutter.repo_name }}/src/main.py
+++ {{ cookiecutter.repo_name }}/src/main.py
@@ -1,7 +1,13 @@
+from .folderA import fileA_func
 
 def main():
     """
     Typical entrypoint to project or pipeline.
     If your pipeline really only has one way to be executed,
-    it is a good idea to string them all together here 
+    it is a good idea to string together all the operations here 
     """
+    fileA_func()
+    return True
+
+if __name__ == "__main__":
+    main()
