7657cc9498013c04241c81803a360def1478e14c
goodshawn12 <goodshawn12@gmail.com>
2023-05-16 18:26:40 -0700

    remove unwanted folders and files

--- {{ cookiecutter.repo_name }}/docs/conf.py
+++ {{ cookiecutter.repo_name }}/docs/conf.py
@@ -26,7 +26,7 @@ import sys
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
---
+++ {{ cookiecutter.repo_name }}/notebooks/sandbox/README.md
@@ -0,0 +1,4 @@
+This directory is a sandbox for you to play in. Anything written here will
+NOT be committed to the repository. Please consider migrating any valuable
+code from the sandbox to another location within the repository when
+ready for review.
