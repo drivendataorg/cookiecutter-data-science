2201994ad5764de525ef72d454105a9da1150046
mahkh6 <mahmoud.khleif@vgregion.se>
2023-08-22 15:48:39 +0200

    AI-1154: updating

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/__init__.py
@@ -1,4 +1,4 @@
-"""init_py}}"""
+"""init-py"""
 import sys
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
@@ -7,17 +7,17 @@ from loguru import logger
 DOTENV_PATH = find_dotenv()
 load_dotenv(DOTENV_PATH)
 
-FMT_ = "{time:YYYY-MM-DD HH:mm:ss} "\
+FMT = "{time:YYYY-MM-DD HH:mm:ss} "\
     "| {level} | {name} :: {module} :: {function} :: {line} - {message}"
 
 
-CONFIG_ = {
+CONFIG = {
     "handlers": [
         {"sink": sys.stdout, "level": "DEBUG"},
-        {"sink": "log.log", "format": FMT_, "level": "DEBUG"},
+        {"sink": "log.log", "format": FMT, "level": "DEBUG"},
     ]
 }
 
-logger.configure(**CONFIG_)
+logger.configure(**CONFIG)
 
 SOURCE_PATH = Path(__file__).resolve().parents[1]
