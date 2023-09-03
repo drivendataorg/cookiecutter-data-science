0962c13937be048856ec3483cf6c22292f02e297
Jay Qi <jayqi@users.noreply.github.com>
2023-09-02 22:29:19 -0400

    Clean up

--- tests/conda_harness.sh
+++ tests/conda_harness.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ex
 
-# Default value if CONDA_EXECUTABLE not specified
-: "${CONDA_EXECUTABLE:=conda}"
-
 # enable conda commands inside the script
 eval "$(conda shell.bash hook)"
 
@@ -13,32 +10,27 @@ CCDS_ROOT=$(dirname $0)
 # configure exit / teardown behavior
 function finish {
     if [[ $(which python) == *"$PROJECT_NAME"* ]]; then
-        conda deactivate  # Mamba still has you use conda activate/deactivate
+        conda deactivate
     fi
-
-    $CONDA_EXECUTABLE env remove -n $PROJECT_NAME -y
+    
+    conda env remove -n $PROJECT_NAME -y
 }
 trap finish EXIT
 
 # source the steps in the test
 source $CCDS_ROOT/test_functions.sh
 
-# navigate to the generated project and run make commands
+# navigate to the generated project and run make commands 
 cd $1
 
 # Fix for conda issue https://github.com/conda/conda/issues/7267 on MacOS
-if [ -e /usr/local/miniconda ]; then
+if [ -e /usr/local/miniconda ]
+then
     sudo chown -R $USER /usr/local/miniconda
 fi
 
-echo "Creating environment..."
-make create_environment CONDA_EXECUTABLE=$CONDA_EXECUTABLE
+make create_environment
+conda activate $PROJECT_NAME
+make requirements
 
-echo "Activating environment..."
-conda activate $PROJECT_NAME  # Mamba still has you use conda activate/deactivate
-
-echo "Installing requirements..."
-make requirements CONDA_EXECUTABLE=$CONDA_EXECUTABLE
-
-echo "Running tests..."
 run_tests $PROJECT_NAME
