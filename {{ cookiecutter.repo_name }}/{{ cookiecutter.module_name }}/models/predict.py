7fa5f04d89df403d1f59f4ba61b93be186481408
Jay Qi <jayqi@users.noreply.github.com>
2024-04-09 21:46:20 -0400

    Can't believe E266 is a thing

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/models/predict.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/models/predict.py
@@ -15,13 +15,13 @@ def main(
     model_path: Path = MODELS_DIR / "some_model.pkl",
     predictions_path: Path = PROCESSED_DATA_DIR / "some_predictions.csv",
 ):
-    #### REPLACE THIS WITH YOUR OWN CODE ####
+    # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Performing inference for some model...")
     for i in tqdm(range(10), total=10):
         if i == 5:
             logger.info("Something happened for iteration 5.")
     logger.success("Inference complete.")
-    #########################################
+    # -----------------------------------------
 
 
 if __name__ == "__main__":
--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/models/train.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/models/train.py
@@ -15,13 +15,13 @@ def main(
     labels_path: Path = PROCESSED_DATA_DIR / "some_labels.csv",
     model_path: Path = MODELS_DIR / "some_model.pkl",
 ):
-    #### REPLACE THIS WITH YOUR OWN CODE ####
+    # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Training some model...")
     for i in tqdm(range(10), total=10):
         if i == 5:
             logger.info("Something happened for iteration 5.")
     logger.success("Modeling training complete.")
-    #########################################
+    # -----------------------------------------
 
 
 if __name__ == "__main__":
