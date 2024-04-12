d9d0f92df5c0b4f4c981f62bd87535a359d48955
Jay Qi <jayqi@users.noreply.github.com>
2024-04-12 14:06:26 -0400

    Get rid of editable check

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/config.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/config.py
@@ -1,6 +1,3 @@
-import importlib.metadata
-import json
-import os
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -10,35 +7,10 @@ from loguru import logger
 # Load environment variables from .env file if it exists
 load_dotenv()
 
-
-# Check if the package is installed as editable
-def _is_editable():
-    # https://peps.python.org/pep-0660/#frontend-requirements
-    try:
-        dist = importlib.metadata.distribution("{{ cookiecutter.module_name }}")
-        direct_url_data = dist.read_text("direct_url.json")
-        if direct_url_data is None:
-            return False
-        return json.loads(direct_url_data).get("dir_info", {}).get("editable", False)
-    except importlib.metadata.PackageNotFoundError:
-        return False
-
-
-IS_EDITABLE = _is_editable()
-
-# Determine PROJ_ROOT path
-if os.getenv("PROJ_ROOT"):
-    logger.debug("Reading PROJ_ROOT from environment variable.")
-    PROJ_ROOT = Path(os.getenv("PROJ_ROOT"))
-elif IS_EDITABLE:
-    logger.debug("Setting PROJ_ROOT relative to editable package.")
-    PROJ_ROOT = Path(__file__).resolve().parents[1]
-else:
-    logger.debug("Using current working directory as PROJ_ROOT.")
-    PROJ_ROOT = Path.cwd()
+# Paths
+PROJ_ROOT = Path(__file__).resolve().parents[1]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
-## Paths
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
