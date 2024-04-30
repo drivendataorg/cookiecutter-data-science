6c6f8879856d25a7cafbb9a18dc409593ff70873
stellarshenson <konrad.jelen@gmail.com>
2024-04-30 15:10:17 +0000

    implemented library-centric cookiecutter template

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,10 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='{{ cookiecutter.library_name }}',
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+    install_requires=[]
 )
