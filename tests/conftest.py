c7683def9299765eb647ce043d16dc45f44723ef
GatlenCulp <GatlenCulp@gmail.com>
2024-12-20 21:41:48 -0500

    Updated conftest to have names on their configs

--- tests/conftest.py
+++ tests/conftest.py
@@ -106,12 +106,17 @@ def fast(request):
 
 
 def pytest_generate_tests(metafunc):
-    # setup config fixture to get all of the results from config_generator
+    """setup config fixture to get all of the results from config_generator"""
+    
+    def make_test_id(config):
+        return f"{config['environment_manager']}-{config['dependency_file']}-{config['docs']}"
+    
     if "config" in metafunc.fixturenames:
         metafunc.parametrize(
-            "config", config_generator(metafunc.config.getoption("fast"))
+            "config", 
+            config_generator(metafunc.config.getoption("fast")),
+            ids=make_test_id
         )
-
 
 @contextmanager
 def bake_project(config):
