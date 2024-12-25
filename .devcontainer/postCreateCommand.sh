ca00d9da711b01427ffb2a67147849a7b8c02c47
GatlenCulp <GatlenCulp@gmail.com>
2024-12-25 02:56:35 -0500

    Added more extensions, configuration on dev container, added versions to pyproject.toml

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,11 +1,9 @@
 #! /usr/bin/env bash
 
 # Install Dependencies
-make create_environment
-make requirements
-# uv pip install -r dev-requirements.txt
+# make create_environment
+# make requirements
+source './.venv/bin/activate'
 
-# source './.venv/bin/activate'
-
-# # Install pre-commit hooks
+# Install pre-commit hooks
 # uv run pre-commit install --install-hooks
