024b46dca6d6082f010729dc0a717e706b05a1e7
GatlenCulp <GatlenCulp@gmail.com>
2024-12-25 04:33:58 -0500

    Finally figured out how the heck to get two virtual environments at once with UV

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,7 @@
 #! /usr/bin/env bash
 
 # Install Dependencies
-# make create_environment
-# make requirements
-source './.venv/bin/activate'
+uv sync --no-cache
 
 # Install pre-commit hooks
 # uv run pre-commit install --install-hooks
