0a26bdb348d77f86da407cc3bd81a70876e53d4c
GatlenCulp <GatlenCulp@gmail.com>
2024-12-24 23:04:36 -0500

    Reformatted files to match black/isort

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
-from contextlib import contextmanager
-from itertools import cycle, product
 import json
-from pathlib import Path
 import shutil
 import sys
 import tempfile
+from contextlib import contextmanager
+from itertools import cycle, product
+from pathlib import Path
 
 import pytest
 
@@ -14,41 +14,48 @@ CCDS_ROOT = Path(__file__).parents[1].resolve()
 
 
 default_args = {
-    'project_name': 'my_test_project',
-    'repo_name': 'my-test-repo',
-    'module_name': 'project_module',
-    'author_name': 'DrivenData',
-    'description': 'A test project',
-    'use_github': 'No',
+    "project_name": "my_test_project",
+    "repo_name": "my-test-repo",
+    "module_name": "project_module",
+    "author_name": "DrivenData",
+    "description": "A test project",
+    "use_github": "No",
 }
 
 
 def config_generator(fast=False):
-    cookiecutter_json = json.load((CCDS_ROOT / 'ccds.json').open('r'))
+    cookiecutter_json = json.load((CCDS_ROOT / "ccds.json").open("r"))
 
     # python versions for the created environment; match the root
     # python version since Pipenv needs to be able to find an executable
     running_py_version = f"{sys.version_info.major}.{sys.version_info.minor}"
-    py_version = [('python_version_number', v) for v in [running_py_version]]
+    py_version = [("python_version_number", v) for v in [running_py_version]]
 
     configs = product(
         py_version,
-        [('environment_manager', opt) for opt in cookiecutter_json['environment_manager']],
-        [('dependency_file', opt) for opt in cookiecutter_json['dependency_file']],
-        [('pydata_packages', opt) for opt in cookiecutter_json['pydata_packages']],
+        [
+            ("environment_manager", opt)
+            for opt in cookiecutter_json["environment_manager"]
+        ],
+        [("dependency_file", opt) for opt in cookiecutter_json["dependency_file"]],
+        [("pydata_packages", opt) for opt in cookiecutter_json["pydata_packages"]],
     )
 
     def _is_valid(config) -> bool:
         config = dict(config)
         # uv is the only one being supported w/o dep file (pyproject.toml only)
-        if (config['dependency_file'] == 'none') and (config['environment_manager'] != 'uv'):
+        if (config["dependency_file"] == "none") and (
+            config["environment_manager"] != "uv"
+        ):
             return False
         #  Pipfile + pipenv only valid combo for either
-        if (config['environment_manager'] == 'pipenv') ^ (config['dependency_file'] == 'Pipfile'):
+        if (config["environment_manager"] == "pipenv") ^ (
+            config["dependency_file"] == "Pipfile"
+        ):
             return False
         # conda is the only valid env manager for environment.yml
-        if (config['dependency_file'] == 'environment.yml') and (
-            config['environment_manager'] != 'conda'
+        if (config["dependency_file"] == "environment.yml") and (
+            config["environment_manager"] != "conda"
         ):
             return False
         return True
@@ -59,10 +66,10 @@ def config_generator(fast=False):
     # cycle over all values other multi-select fields that should be inter-operable
     # and that we don't need to handle with combinatorics
     cycle_fields = [
-        'dataset_storage',
-        'open_source_license',
-        'include_code_scaffold',
-        'docs',
+        "dataset_storage",
+        "open_source_license",
+        "include_code_scaffold",
+        "docs",
     ]
     cyclers = {k: cycle(cookiecutter_json[k]) for k in cycle_fields}
 
@@ -72,7 +79,7 @@ def config_generator(fast=False):
         # Alternate including the code scaffold
         for field, cycler in cyclers.items():
             config[field] = next(cycler)
-        config['repo_name'] += f"-{ind}"
+        config["repo_name"] += f"-{ind}"
         yield config
 
         # just do a single config if fast passed once or three times
@@ -90,17 +97,17 @@ def pytest_addoption(parser):
      -FFF - skip makefile commands, single config
     """
     parser.addoption(
-        '--fast',
-        '-F',
-        action='count',
+        "--fast",
+        "-F",
+        action="count",
         default=0,
-        help='Speed up tests by skipping configs and/or Makefile validation',
+        help="Speed up tests by skipping configs and/or Makefile validation",
     )
 
 
 @pytest.fixture
 def fast(request):
-    return request.config.getoption('--fast')
+    return request.config.getoption("--fast")
 
 
 def pytest_generate_tests(metafunc):
@@ -109,17 +116,17 @@ def pytest_generate_tests(metafunc):
     def make_test_id(config):
         return f"{config['environment_manager']}-{config['dependency_file']}-{config['docs']}"
 
-    if 'config' in metafunc.fixturenames:
+    if "config" in metafunc.fixturenames:
         metafunc.parametrize(
-            'config',
-            config_generator(metafunc.config.getoption('fast')),
+            "config",
+            config_generator(metafunc.config.getoption("fast")),
             ids=make_test_id,
         )
 
 
 @contextmanager
 def bake_project(config):
-    temp = Path(tempfile.mkdtemp(suffix='data-project')).resolve()
+    temp = Path(tempfile.mkdtemp(suffix="data-project")).resolve()
 
     api_main.cookiecutter(
         str(CCDS_ROOT),
@@ -129,7 +136,7 @@ def bake_project(config):
         overwrite_if_exists=True,
     )
 
-    yield temp / config['repo_name']
+    yield temp / config["repo_name"]
 
     # cleanup after
     shutil.rmtree(temp)
