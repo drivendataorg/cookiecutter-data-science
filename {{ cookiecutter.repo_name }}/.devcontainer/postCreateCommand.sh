a8232a843a8ea5e0f3ee1476c820e4f5b419a060
GatlenCulp <GatlenCulp@gmail.com>
2025-02-04 23:57:51 -0500

    refactor(pyproject.toml): Add commitizen and pre-commit to the dev requirements. Disable typical packages for less bloat.

--- {{ cookiecutter.repo_name }}/.devcontainer/postCreateCommand.sh
+++ {{ cookiecutter.repo_name }}/.devcontainer/postCreateCommand.sh
@@ -2,9 +2,5 @@
 
 # Install Dependencies
 uv sync
-uv pip install -r dev-requirements.txt
-
 source './.venv/bin/activate'
-
-# Install pre-commit hooks
-uv run pre-commit install --install-hooks
+pre-commit install
