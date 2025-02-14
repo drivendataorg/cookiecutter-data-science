dc924d3594861edcde3df2615417ad1c765fd1ed
Chris Kucharczyk <chris@drivendata.org>
2025-02-14 22:09:35 +0000

    Update hooks/pre_prompt.py
    Co-authored-by: Robert Gibboni <robert@drivendata.org>

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -3,10 +3,11 @@ import warnings
 from ccds.version import __version__ as version
 
 if __name__ == "__main__":
-    if version <= "2.0.0":
+    if version < "2.0.1":
         warnings.warn(
             "It looks like you're using a version of CCDS that "
-            "always uses the latest template. For more stable behavior, "
-            "update to a newer version of CCDS.",
+            "defaults to using the latest template. For more stable behavior, "
+            "update to CCDS version 2.0.1 or later. To upgrade to the latest "
+            "version, run 'pip install -U cookiecutter-data-science'",
             DeprecationWarning,
         )
