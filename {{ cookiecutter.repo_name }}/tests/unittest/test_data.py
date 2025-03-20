23860b45e2d9561ae593c69de7bb87cc78732105
Robert Gibboni <robert@drivendata.org>
2025-03-20 10:43:45 -0400

    Simple failing tests and make test command

--- {{ cookiecutter.repo_name }}/tests/unittest/test_data.py
+++ {{ cookiecutter.repo_name }}/tests/unittest/test_data.py
@@ -1,1 +1,11 @@
-assert True
+import unittest
+
+
+class TestCodeIsTested(unittest.TestCase):
+
+    def test_code_is_tested(self):
+        self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
