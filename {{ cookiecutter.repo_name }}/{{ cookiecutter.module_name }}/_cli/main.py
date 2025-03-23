f52616c62916bdf59bf6f73d19e7c4eb5ff38005
GatlenCulp <GatlenCulp@gmail.com>
2025-03-23 16:56:04 -0400

    fix cookiecutter errors

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/_cli/main.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/_cli/main.py
@@ -8,8 +8,7 @@ app = typer.Typer()
 
 # {#
 # TODO: Update to have a simple version command that gnabs the version
-# from the pyproject.toml file.
-# }
+# from the pyproject.toml file. #}
 @app.command()
 def hello(name: str = "World") -> None:
     """Say hello to someone.
