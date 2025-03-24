e6119d32d4f8e4f054028f3e371fb98c5484d1e5
Robert Gibboni <robert@drivendata.org>
2025-03-23 20:54:43 -0400

    Resolve python version to work with compatible operator (#446)
    * Resolve python version to work with compatible operator
    
    * Fix version specifier resolution
    
    * Format
    
    * Test virtual environment Python version
    
    Use uv in CI
    
    Fix python version of created environment in tests
    
    Pipenv should create its own virtualenv
    
    Remove sys import
    
    Remove extraneous parenthesis
    
    Fix wrong input to configs product
    
    Fix virtualenvwrapper to set Python version instead of Python interpreter
    
    Install Python executables into PATH
    
    * Revert "Test virtual environment Python version"
    
    This reverts commit f16bcc733cd42eca91b79dca77994c4d84e93d3b.
    
    ---------
    
    Co-authored-by: Jay Qi <jayqi@users.noreply.github.com>

--- ccds/hook_utils/dependencies.py
+++ ccds/hook_utils/dependencies.py
@@ -30,6 +30,49 @@ scaffold = [
 ]
 
 
+def resolve_python_version_specifier(python_version):
+    """Resolves the user-provided Python version string to a version specifier.
+
+    Examples:
+
+    User provides: 3.12
+    Resolved version specifier: ~=3.12.0
+    Compatible versions: 3.12.0, 3.12.1, 3.12.2, etc.
+
+    User provides: 3.12.2
+    Resolved version specifier: ==3.12.2
+    Compatible versions: 3.12.2
+
+    See https://packaging.python.org/en/latest/specifications/version-specifiers/#compatible-release
+    """
+    version_parts = python_version.split(".")
+    if len(version_parts) == 2:
+        major, minor = version_parts
+        patch = "0"
+        operator = "~="
+    elif len(version_parts) == 3:
+        major, minor, patch = version_parts
+        operator = "=="
+    else:
+        raise ValueError(
+            f"Invalid Python version specifier {python_version}. "
+            "Please specify version as <major>.<minor> or <major>.<minor>.<patch>, "
+            "e.g., 3.10, 3.10.1, etc."
+        )
+
+    resolved_python_version = ".".join((major, minor, patch))
+    return f"{operator}{resolved_python_version}"
+
+
+def write_python_version(python_version):
+    with open("pyproject.toml", "r") as f:
+        doc = tomlkit.parse(f.read())
+
+    doc["project"]["requires-python"] = resolve_python_version_specifier(python_version)
+    with open("pyproject.toml", "w") as f:
+        f.write(tomlkit.dumps(doc))
+
+
 def write_dependencies(
     dependencies, packages, pip_only_packages, repo_name, module_name, python_version
 ):
